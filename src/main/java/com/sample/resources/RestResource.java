d65c97df39eff6176aa7e201ae09f325fd5723b3
Totta Wang <totta.wang@autodesk.com>
2017-01-23 13:05:33 +0800

    add the initial implementation

---
+++ src/main/java/com/sample/resources/RestResource.java
@@ -0,0 +1,84 @@
+package com.sample.resources;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.netflix.config.ConfigurationManager;
+import com.sample.conf.HttpWebClient;
+import com.sample.service.HystrixService;
+
+@Component
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/api")
+public class RestResource {
+
+  private static int POOL_SIZE = 1;
+  private static final ExecutorService workers = Executors.newFixedThreadPool(POOL_SIZE);
+
+  @Autowired
+  private HystrixService service;
+
+  @Autowired
+  private HttpWebClient httpWebClient;
+
+  @GET
+  @Path("hystrix-setting")
+  public Object getSettings() {
+    return ConfigurationManager.getConfigInstance()
+        .getProperty(httpWebClient.getHystrixPoolMaxQueueKey());
+  }
+
+  @GET
+  @Path("hystrix-blocking")
+  public String getUserProjects() {
+    String result = "";
+    long start = System.currentTimeMillis();
+    result = service.getContent();
+    long end = System.currentTimeMillis();
+    System.out.println("Time taken to get results " + (end - start) + " milliseconds");
+    return result;
+  }
+
+  @GET
+  @Path("hystrix-blocking-currency")
+  public String getUserProjectsCurrency() throws InterruptedException, ExecutionException {
+
+    String result = "";
+    long start = System.currentTimeMillis();
+
+    // call the service.getContent 30 times in parallel
+    Collection<Callable<String>> tasks = new ArrayList<Callable<String>>();
+    for (int i = 0; i < POOL_SIZE; i++) {
+      tasks.add(new Callable<String>() {
+        public String call() throws Exception {
+          return service.getContent();
+        }
+      });
+    }
+
+    List<Future<String>> results = workers.invokeAll(tasks, 500, TimeUnit.SECONDS);
+    for (Future<String> f : results) {
+      result += f.get();
+    }
+
+    long end = System.currentTimeMillis();
+    System.out.println("Time taken to get results " + (end - start) + " milliseconds");
+    return result;
+  }
+
+}
---
+++ src/main/java/com/sample/service/HystrixService.java
@@ -0,0 +1,54 @@
+package com.sample.service;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.sample.conf.Application;
+import com.sample.conf.HttpWebClient;
+
+@Component
+public class HystrixService {
+
+  @Value("${HYSTRIX_TEST_URL:url_not_accessiable}")
+  private String HYSTRIX_TEST_URL;
+
+  @Autowired
+  @Qualifier(Application.DEFAULT_REST_TEMPLATE)
+  protected RestTemplate restTemplate;
+
+  @HystrixCommand(groupKey = HttpWebClient.GROUP, commandKey = HttpWebClient.COMMAND_GET,
+      threadPoolKey = HttpWebClient.THREAD_POOL_KEY,
+      ignoreExceptions = {IgnoreRuntimeException.class})
+  public String getContent() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
+    HttpEntity<String> entity = new HttpEntity<String>(headers);
+    try {
+      URI endpointUrl = new URI(HYSTRIX_TEST_URL);
+      ResponseEntity<String> responseEntity =
+          restTemplate.exchange(endpointUrl, HttpMethod.GET, entity, String.class);
+      return responseEntity.getBody();
+    } catch (ResourceAccessException | HttpServerErrorException ex) {
+      // Server side exception is a proper case for circuit breaker, hystrix will translate
+      // ServiceUnavailableException to HystrixRuntimeException which indicates a system failure.
+      throw new ServiceUnavailableException("Service is not available", ex);
+    } catch (Throwable ex) {
+      // any non sever side exception should be ignored by Hystirx so that no
+      // HystrixRuntimeException can be thrown
+      throw new IgnoreRuntimeException(
+          "client exception, can be ingored for hystrix circuit breaker: " + ex.getMessage(), ex);
+    }
+  }
+}
